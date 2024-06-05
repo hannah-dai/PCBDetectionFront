45c7679e85bd7906ffbb78557c99ebe6c1f0d31c
wzdszlzczmzmn <120558315+wzdszlzczmzmn@users.noreply.github.com>
2024-06-05 19:27:12 +0800

    Fix a Problem

---
+++ pages/dataShow/dataDisplay/dataDisplay.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="backView">
+		<uv-sticky>......</uv-sticky>
+		<view class="timeSelect">
+			TimeSelect
+		</view>
+		<view class="timeSelect">
+			TimeSelect
+		</view>
+		<view class="timeSelect">
+			TimeSelect
+		</view>
+		<view class="timeSelect">
+			TimeSelect
+		</view>
+		<view class="timeSelect">
+			TimeSelect
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style>
+	.backView {
+		background-color: rgb(241 245 249);
+		min-height: 100%;
+		padding-top: 15px;
+	}
+	.timeSelect {
+		background-color: #fff;
+		height: 200px;
+		width: 90%;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 15px;
+		border-radius: 0.5rem;
+	}
+</style>
